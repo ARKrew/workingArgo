e560b1c0b09a2160122df48712dfd3ea27ac5a2f
ykeanu <ykhernan@ucla.edu>
2017-12-08 00:15:29 -0800

    Render badge onto modal

--- src/components/screens/ListScreens/MainSceneAR.js
+++ src/components/screens/ListScreens/MainSceneAR.js
@@ -11,20 +11,28 @@ import {
   Image
 } from 'react-native';
 import { connect } from 'react-redux';
-import {
-  ViroARSceneNavigator
-} from 'react-viro';
-import { NavigationActions, addNavigationHelpers } from 'react-navigation';
+import { ViroARSceneNavigator } from 'react-viro';
+import { 
+  NavigationActions, 
+  addNavigationHelpers 
+} from 'react-navigation';
 import DemoARPortal from './DemoARPortal';
-// import { listNavigate } from '../../../actions/ListNavActions';
-import { listNavigate, enterAR } from '../../../actions';
 // import { AppNavigator } from '../../../AppNavigator';
 import ListNav from './ListNav';
-import { routerInitialState, router, ROUTES } from '../../../AppNavigator';
+import { 
+  routerInitialState, 
+  router, 
+  ROUTES 
+} from '../../../AppNavigator';
 import { Button } from '../../../components/common';
+import { 
+  listNavigate, 
+  enterAR, 
+  updateDisplayBadge 
+} from '../../../actions';
 
 const sharedProps = {
-    apiKey:"D5FCCB74-1B13-4E50-BCE8-3DAE6B9ED443"
+    apiKey: 'D5FCCB74-1B13-4E50-BCE8-3DAE6B9ED443'
   };
 
   class MainSceneAR extends Component {
@@ -46,6 +54,17 @@ const sharedProps = {
       this.props.navigation.navigate('List');
       this.props.listNavigate();
     }
+
+    renderDisplayBadge() {
+      return (
+        <Image
+        style={styles.badge}
+        key={this.props.badge.displayBadge.fileName}
+        source={this.props.badge.displayBadge.image}
+        />
+      );
+    }
+
     // Replace this function with the contents of _getDemoSceneARNavigator() or _getMainSceneARNavigator()
     // if you are building a specific type of experience.
     render() {
@@ -68,11 +87,12 @@ const sharedProps = {
       }
         return (
           <TouchableOpacity >
-            <Text style={{ color: "black", fontSize: 22, marginTop: 35, fontFamily: 'Lato-Regular' }}>
+            <Text style={{ color: 'black', fontSize: 22, marginTop: 35, fontFamily: 'Lato-Regular' }}>
               Congrats you picked up your first badge!
               Now head to your Profile Page to see your new badge!
             </Text>
             <Button onPress={this.exitAR}>Profile Page</Button>
+            {this.renderDisplayBadge()}
         </TouchableOpacity>
         );
     }
@@ -96,6 +116,11 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     borderWidth: 1,
     borderColor: '#ffffff00',
+  },
+  badge: {
+    width: 70,
+    height: 70,
+    margin: 5
   }
 });
 
@@ -103,10 +128,13 @@ const mapStateToProps = state => {
   return {
     ARstate: state.demoAR,
     navState: state.nav,
-    currentRoute: state.nav.routes[1].index
+    currentRoute: state.nav.routes[1].index,
+    badge: state.badge
   };
 };
 
-export default connect(mapStateToProps, { listNavigate, enterAR })(MainSceneAR);
-
-  // module.export = MainSceneAR;
+export default connect(mapStateToProps, { 
+  listNavigate, 
+  enterAR,
+  updateDisplayBadge
+})(MainSceneAR);
