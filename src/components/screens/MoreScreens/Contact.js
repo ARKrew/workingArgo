d4786eab189d1736e4e6ef5b88c9760811ce5120
Donovan Lowkeen <donovanlowkeen@Donovans-MacBook-Pro.local>
2017-12-09 12:09:34 -0800

    Created email functionality

--- src/components/screens/MoreScreens/Contact.js
+++ src/components/screens/MoreScreens/Contact.js
@@ -1,14 +1,37 @@
-import React from "react";
-import { View, Text } from "react-native";
-import { Header } from "../../common";
+import React from 'react';
+import { View, Text } from 'react-native';
+import Communications from 'react-native-communications';
+import { Header, Button, CardSection } from '../../common';
 
 const Contact = () => {
-  return (
-    <View>
+  return <View>
       <Header headerText="Contact" />
-      <Text>Here is some text for this page.</Text>
-    </View>
-  );
+      <Text>Please send us any questions, comments, or concerns</Text>
+      <CardSection>
+        <Button
+          onPress={() =>
+            Communications.email(
+              ["arkrewucla@gmail.com"],
+              ["lowkeendonovan@gmail.com", "lugraciecy@gmail.com"],
+              null,
+              "ARgo User",
+              "Talk to us!"
+            )
+          }
+        >
+          Email Us!
+        </Button>
+      </CardSection>
+      <CardSection>
+        <Button
+          onPress={() =>
+            Communications.web("https://github.com/ARKrew/workingArgo")
+          }
+        >
+          View GitHub Repo
+        </Button>
+      </CardSection>
+    </View>;
 };
 
 export default Contact;
